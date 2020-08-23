7449c4a8ddbd8a063f913159fe5ed819f327dc37
sudelearning <1079323801@qq.com>
2020-08-23 23:24:54 +0800

    it is a test21!

---
+++ src/main/java/com/heima/demospringboot/Main.java
@@ -0,0 +1,16 @@
+package com.heima.demospringboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@RestController
+public class Main {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Spring Boot";
+    }
+
+}
